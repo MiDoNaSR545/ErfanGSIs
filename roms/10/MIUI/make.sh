77703fb0e074ed4ef497849ec9fb4dbe28de416c
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-02-07 10:54:24 +0200

    Update make.sh

--- roms/10/MIUI/make.sh
+++ roms/10/MIUI/make.sh
@@ -12,10 +12,7 @@ rm -rf $1/priv-app/FindDevice
 # Copy system files
 rsync -ra $thispath/system/ $systempath
 
-#fix systemui crash because of FOD
-echo "ro.hardware.fp.fod=true" >> $1/build.prop
-echo "persist.vendor.sys.fp.fod.location.X_Y=445,1260" >> $1/build.prop
-echo "persist.vendor.sys.fp.fod.size.width_height=190,190" >> $1/build.prop
+#fix systemui crash
 echo "DEVICE_PROVISIONED=1" >> $1/build.prop
 
 # drop dirac
@@ -26,8 +23,6 @@ rm -rf $1/app/FingerprintExtensionService
 rm -rf $1/app/NQNfcNci
 
 cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
-
-sed -i 's/<bool name="support_round_corner">true/<bool name="support_round_corner">false/' $1/etc/device_features/*
 
 sed -i "/miui.notch/d" $1/build.prop
 
